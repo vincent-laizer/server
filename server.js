bf7db6006b26e0cb8b8c5691d52c85d962844b6b
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-02-10 16:01:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+cont http = require('http');
+const port=process.env.PORT || 3000
+
 var express = require('express');
 var app = express();
 
 app.get('/:name', function(req, res){
    res.send('Name specified is: ' + req.params.name);
 });
-app.listen(3000);
+app.listen(port);
+
