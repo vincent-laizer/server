b3c9e6c97f942210cc02fee1e42a058e337305a3
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-02-10 15:54:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const http = require('http');
-const port=process.env.PORT || 3000
+var express = require('express');
+var app = express();
 
-const server = http.createServer((req, res) => {
-
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+app.get('/:name', function(req, res){
+   res.send('Name specified is: ' + req.params.name);
 });
-
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+app.listen(3000);
