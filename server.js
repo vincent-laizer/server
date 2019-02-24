46304b6b30b037896acf275c53a6aa789d4efcdb
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-02-24 16:35:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 //1.
 var http = require('http');
 var fs = require('fs');
+var urls = require('url');
+
 //2.
 const port=process.env.PORT || 3000
 var server = http.createServer(function (req, resp) {
+    
     //3.
-    if (req.url === "/") {
-        fs.readFile("data.txt", function (error, pgResp) {
+    if (req.url) {
+        
+            //obtain data from query string
+            var query = urls.parse(req.url, true).query;
+            var fileName = query.file;
+        
+        fs.readFile(fileName, function (error, pgResp) {
             if (error) {
                 resp.writeHead(404);
                 resp.write('Contents you are looking are Not Found');
             } else {
                 resp.writeHead(200, { 'Content-Type': 'text/plain' });
                 resp.write("You are accessing this content from: " + req.url + "\n");
+                resp.write(fileName);
                 resp.write(pgResp);
             }
              
