1b0be6bbcdd76ddf0275bb49e2e154a731f283e2
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-03-10 16:00:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port=process.env.PORT || 3000
 var server = http.createServer(function (req, resp) {
     //3.
     if (req.url === "/") {
-        fs.readFile("FemaBoard.html", function (error, pgResp) {
+        fs.readFile("index", function (error, pgResp) {
             if (error) {
                 resp.writeHead(404);
                 resp.write('Contents you are looking are Not Found');
