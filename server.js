dd6a62d32065683692c59cfb6ceeee7a39f45576
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-03-10 15:24:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 var http = require("http");
-var http = require('http');
-var url = require('url');
+var url = require("url");
 
 //variable system defined port
 var port = process.env.port||3000;
 
-//creating the server
 http.createServer(function(req,res){
 
-    res.writeHead(200,{
-        'content-Type':"text/html"
-    });
-    res.write("its working!!");
-    res.end();
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-  res.end(txt);
+    res.writeHead(200,{"content-type":"text/html"});
+    res.end("hello");
+    
 }).listen(port);
