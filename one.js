eaecf230c1ff91fbc2ddda1fee796cf985a0e87d
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-03-10 15:58:24 +0300

    Update and rename one.js to server.js

--- one.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-
-const port = process.env.port||3000;
-
-http.createServer(function(req,res){
-
-  res.writeHead(200);
-  
-}).listen(port);
---
+++ server.js
@@ -0,0 +1,30 @@
+//1.
+var http = require('http');
+var fs = require('fs');
+//2.
+const port=process.env.PORT || 3000
+var server = http.createServer(function (req, resp) {
+    //3.
+    if (req.url === "/") {
+        fs.readFile("FemaBoard.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+             
+            resp.end();
+        });
+    } else {
+        //4.
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
+    }
+});
+//5.
+server.listen(port);
+ 
+console.log('Server Started listening on 5050');
