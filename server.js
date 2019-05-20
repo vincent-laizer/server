5dc03c3f91ed38cc543bd54c22358540b1df4a39
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-05-20 15:17:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ var server = http.createServer(function (req, resp) {
         fs.readFile("yst", function (error, pgResp) {
             if (error) {
                 resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
             } else {
                 resp.writeHead(200);
                 resp.write(prResp);
