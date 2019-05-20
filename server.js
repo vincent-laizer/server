83db5a218edca95102bef5dc77bf245064ae4630
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-05-20 15:14:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ var server = http.createServer(function (req, resp) {
                 resp.writeHead(404);
                 resp.write('Contents you are looking are Not Found');
             } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write("<h2>Thank you for downloading</h2>");
+                resp.writeHead(200);
+                resp.write(prResp);
             }
              
             resp.end();
