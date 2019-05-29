4b442979cd732a0a467154f0af836a8c6053a80e
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-05-29 16:48:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var fs = require("fs");
 const port = process.env.port||3000;
 
 http.createServer(function(req,res){
-  res.write(getDate());
+  res.write(Date.now());
   res.end();
 }).listen(port);
