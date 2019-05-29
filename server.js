ae4be08625fe8d18a2162ef0ec5aa7ee868a09b6
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-05-29 16:45:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 
-var port = process.env.port||3000;
+const port = process.env.port||3000;
 
 http.createServer(function(req,res){
   res.write(getDate());
