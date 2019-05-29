f4d915ac0d8cfa4d22396c62a21bdf955f65df49
vincent0x1 <47497857+vincent0x1@users.noreply.github.com>
2019-05-29 16:41:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+var http = require("http");
+var fs = require("fs");
 
+var port = process.env.port||3000;
+
+http.createServer(function(req,res){
+  res.write("working...");
+  res.end();
+}).listen(port);
